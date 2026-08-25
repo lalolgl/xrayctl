67fe9e152ba575dcf98d449024b792bea6ef1719
AOG <flava.hustle.hard@gmail.com>
2026-08-25 09:35:43 +0300

    Update flakes

--- flake.nix
+++ flake.nix
@@ -1,24 +1,38 @@
 {
-  description = "Development environment for xrayctl";
+  description = "Xray subscription and profile management CLI";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs =
+    { nixpkgs, ... }:
     let
       system = "x86_64-linux";
+
       pkgs = import nixpkgs {
         inherit system;
       };
     in
     {
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "xrayctl";
+        version = "0.1.0";
+
+        src = ./.;
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+      };
+
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
           rustc
           cargo
           gcc
           rustfmt
+          clippy
         ];
       };
     };
